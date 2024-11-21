cd97e8432e4c802cf642da3f98bbc3c8fdc0805a
Joshua "Shadow" Monson <4204089+ShadowLordAlpha@users.noreply.github.com>
2024-11-21 01:57:28 -0700

    Correct Lower Paddle Collision
    When colliding with the lower wall the paddle would incorrectly go to far before stopping. This fixes the order of operations to correct when the paddle stops

--- Chapter01/Game.cpp
+++ Chapter01/Game.cpp
@@ -141,9 +141,9 @@ void Game::UpdateGame()
 		{
 			mPaddlePos.y = paddleH/2.0f + thickness;
 		}
-		else if (mPaddlePos.y > (768.0f - paddleH/2.0f - thickness))
+		else if (mPaddlePos.y > (768.0f - (paddleH/2.0f - thickness)))
 		{
-			mPaddlePos.y = 768.0f - paddleH/2.0f - thickness;
+			mPaddlePos.y = 768.0f - (paddleH/2.0f - thickness);
 		}
 	}
 	
